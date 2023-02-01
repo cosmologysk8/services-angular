dc2a5db1517fbfb4a45cf9deb3cd2286018a78f6
cosmologysk8 <a1999afg@gmail.com>
2023-02-01 13:10:06 +0100

    Terminado los Services

---
+++ src/app/account.service.ts
@@ -0,0 +1,36 @@
+import {LoggingService} from "./logging.service";
+import {EventEmitter, Injectable} from "@angular/core";
+@Injectable({providedIn: 'root'})
+export class AccountService {
+
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
+    }
+  ];
+
+  statusUpdate = new EventEmitter<string>();
+
+  constructor(private logginService: LoggingService) {
+  }
+
+  addAccount(name: string, status: string){
+    this.accounts.push({name: name, status: status});
+    this.logginService.logStatusChange(status)
+  }
+
+  updateStatus(id : number, status: string){
+    this.accounts[id].status = status;
+    this.logginService.logStatusChange(status);
+  }
+
+}
--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,18 +1,27 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {LoggingService} from "../logging.service";
+import {AccountService} from "../account.service";
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
+  // providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+  // Los services se inyectan a los componentes através del constructor
+  constructor(private loggingService : LoggingService, private accountService : AccountService) {
+    this.accountService.statusUpdate.subscribe(
+      (status : string) => alert('New Status: ' + status)
+    );
+  }
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.accountService.updateStatus(this.id, status);
+    // this.loggingService.logStatusChange(status);
+    this.accountService.statusUpdate.emit(status);
   }
 }
